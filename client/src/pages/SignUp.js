28f070e64921e957f57771a2bc7a0ec78a761bd1
Priya V <priyav2.bct@gmail.com>
2024-05-29 06:50:35 +0530

    Created Signup UI & Function

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,5 +1,82 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(false);
+    axios
+      .post("/api/auth/signup", formData)
+      .then(() => {
+        console.log("Data added successfully");
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log("This is  Error", err.response.data);
+        setLoading(false);
+        setError(true);
+        console.log(err);
+      });
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="userName"
+          className="bg-slate-100 p-3 rounded-lg"
+          autoComplete="true"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          autoComplete="true"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500">Sign in</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something went Wrong"}</p>
+    </div>
+  );
 }
+
+// const res = await fetch("/api/auth/signup", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// });
+// const data = await res.json();
+// console.log(data);
--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -3,12 +3,15 @@ import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
   const { userName, email, password } = req.body;
+  console.log(req.body); // Add this line to log the request body
+
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ userName, email, password: hashedPassword });
+  const newUser = new User({ userName, email, password });
   try {
     await newUser.save();
     res.status(201).json("Saved Successfully");
   } catch (error) {
+    console.error("Error during signup:", error);
     next(error);
   }
 };
